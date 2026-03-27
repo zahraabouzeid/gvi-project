bd3265d4ec7778cf27eff1a54a7d521fa59822ab
bwv-sk <keussas17@schueler.bwv-ac.de>
2026-03-27 08:36:18 +0100

    TopicAreas angelegt

--- src/main/java/com/gvi/project/models/questions/TopicArea.java
+++ src/main/java/com/gvi/project/models/questions/TopicArea.java
@@ -1,12 +1,17 @@
 package com.gvi.project.models.questions;
 
 public enum TopicArea {
-    SQL_GRUNDLAGEN("Datenbank - SQL"),
-    SELECT_ABFRAGEN("Wirtschaft"),
-    NORMALISIERUNG("Recht"),
-    ER_MODELLIERUNG("Datenbanken Modellierung / UML"),
-    DDL_DML("Programmierung Pseudocode"),
-    JOINS_SUBQUERIES("Maschinelles Lernen");
+    SQL_GRUNDLAGEN("SQL Grundlagen"),
+    SELECT_ABFRAGEN("SELECT & Abfragen"),
+    NORMALISIERUNG("Normalisierung"),
+    ER_MODELLIERUNG("ER-Modellierung"),
+    DDL_DML("DDL & DML"),
+    JOINS_SUBQUERIES("Joins & Subqueries"),
+    WIRTSCHAFT("Wirtschaft"),
+    RECHT("Recht"),
+    DB_MODELLIERUNG("Datenbanken Modellierung / UML"),
+    PSEUDOCODE("Programmierung Pseudocode"),
+    MASCHINELLES_LERNEN("Maschinelles Lernen");
 
     private final String displayName;
 
